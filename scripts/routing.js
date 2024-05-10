ef05339b5272895286526153d95a50ff11d084a1
Nate Johnson <natejohnson0592@gmail.com>
2024-05-10 15:39:47 -0500

    added hot-coffee styles, script and html

---
+++ scripts/routing.js
@@ -0,0 +1,14 @@
+function goToHot() {
+  window.location.href = "/hotcoffee.html";
+  return true;
+}
+
+function goToCold() {
+  window.location.href = "/coldcoffee.html";
+  return true;
+}
+
+function goToHome() {
+  window.location.href = "/index.html";
+  return true;
+}
