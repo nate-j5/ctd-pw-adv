2ad9761ad56ef720637393a59901dc524a5b67bf
Nate Johnson <natejohnson0592@gmail.com>
2024-05-11 11:06:12 -0500

    added cold recipes

---
+++ scripts/coldScript.js
@@ -0,0 +1,66 @@
+let currentIndex = 0;
+let coldRecipes = [];
+let currentImageUrl = "";
+
+// Fetch recipes from the API
+function fetchRecipes() {
+  fetch("https://api.sampleapis.com/coffee/iced")
+    .then((response) => response.json())
+    .then((recipe) => {
+      coldRecipes = recipe;
+      renderRecipes();
+      renderArrows();
+    })
+    .catch((error) => {
+      console.error("Error fetching recipes:", error);
+    });
+}
+
+// Render the current recipe
+function renderRecipes() {
+  const recipe = coldRecipes[currentIndex];
+  const recipeContainerList = document.getElementById("recipe-list");
+  const ingredientsList = recipe.ingredients
+    .map((ingredient) => `<li>${ingredient}</li>`)
+    .join("");
+
+  currentImageUrl = recipe.image;
+
+  recipeContainerList.innerHTML = `<h2>${recipe.title}</h2>
+    <p>${recipe.description}</p>
+    <p id="ingredientID">Ingredients:</p>
+    <ul>${ingredientsList}</ul>`;
+
+  const currentImageElement = document.getElementById("current-image");
+  currentImageElement.src = currentImageUrl;
+}
+
+// Render the left and right arrows
+function renderArrows() {
+  const arrowsContainer = document.getElementById("arrows");
+  arrowsContainer.innerHTML = `
+        <button id="prev-btn">←</button>
+        <button id="next-btn">→</button>
+    `;
+
+  // Add event listeners to arrow buttons
+  const prevButton = document.getElementById("prev-btn");
+  const nextButton = document.getElementById("next-btn");
+  prevButton.addEventListener("click", prevItem);
+  nextButton.addEventListener("click", nextItem);
+}
+
+// Cycle to the next user
+function nextItem() {
+  currentIndex = (currentIndex + 1) % coldRecipes.length;
+  renderRecipes();
+}
+
+// Cycle to previous user
+function prevItem() {
+  currentIndex = (currentIndex - 1 + coldRecipes.length) % coldRecipes.length;
+  renderRecipes();
+}
+
+// Call the fetchcoldRecipes when the DOM is loaded
+document.addEventListener("DOMContentLoaded", fetchRecipes);
